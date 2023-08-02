4d724c7634106c011ea687fab1d8e576b3f10f66
nickrosario100 <nick.r.rosario@gmail.com>
2023-08-02 19:03:02 -0400

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,98 @@
+const logotext = "Personal Trainer";
+const meta = {
+    title: "Carlos Maccay",
+    description: "Desc",
+};
+
+const introdata = {
+    title: "I’m Carlos Macaay",
+    animated: {
+        first: "I am a Fintess Trainer",
+        second: "I am gay",
+        third: "I develop mobile apps",
+    },
+    description: "dsk.",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "My background",
+    aboutme: "Experience.",
+};
+const worktimeline = [{
+        jobtitle: "Plan Type",
+        where: "Details:",
+        date: "Cost:",
+    },
+    {
+        jobtitle: "Bronze",
+        where: "",
+        date: "$50 1/2 Hr.",
+    },
+    {
+        jobtitle: "Silver",
+        where: "",
+        date: "$75 Hr.",
+    },
+    {
+        jobtitle: "Gold",
+        where: "I train with you.",
+        date: "$100 Hr.",
+    },
+    {
+        jobtitle: "Online",
+        where:"Pdf file with all workouts including Sets, Reps and video guides",
+        date: "$75 Per Course",
+    },
+    //add times and
+    //
+];
+
+const skills = [{
+        name: "Cardio",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Placeholder",
+        description: "random",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
