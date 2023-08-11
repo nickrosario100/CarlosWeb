9c1c376b13f34052b6722c921b4ab5ee7391e2f2
nickrosario100 <nick.r.rosario@gmail.com>
2023-08-11 10:50:54 -0400

    plans cards

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import {
   dataabout,
   meta,
@@ -36,7 +37,7 @@ export const About = () => {
             </div>
           </Col>
         </Row>
-        <Row className=" sec_sp">
+        {/* <Row className=" sec_sp">
           <Col lg="5">
             <h1 className="color_sec py-4">Plans</h1>
           </Col>
@@ -59,7 +60,7 @@ export const About = () => {
             </table>
           </Col>
           
-        </Row>
+        </Row> */}
         {/* <Row className="sec_sp">
           <Col lg="5">
             <h3 className="color_sec py-4">Skills</h3>
@@ -84,7 +85,7 @@ export const About = () => {
             })}
           </Col>
         </Row> */}
-        <Row className="sec_sp">
+        {/* <Row className="sec_sp">
           <Col lang="5">
             <h1 className="color_sec py-4">Plans</h1>
           </Col>
@@ -102,51 +103,66 @@ export const About = () => {
               );
             })}
           </Col>
-        </Row>
-        <Row>
-        <div className="mb-5 pc_items_ho">
-          {plans.map((data, i) => {
+        </Row> */}
+        <div className="container3">
+        <Row className="container2">
+        {plans.map((data, i) => {
             return (
-              <div key={i} className="pc_item">
-                <img src={data.picture} alt="" />
-                <div className="content">
-                <h1 className="service_desc">{data.title}</h1>
-                <p className="service_desc">{data.description}</p>
-                  <p className="service_desc">{data.sessions}</p>
-                  <p className="service_desc">{data.timePerSession}</p>
-                  <p className="service_desc">{data.cost}</p>
-                </div>
-              </div>
-            );
+        <div key={i} class="mx-3 mb-4 pack-container text ">
+  <div class="header">
+    <p class="title">
+    {data.title}
+    </p>
+    <div class="price-container">
+      <span>$</span>{data.cost}
+      <span>/mo</span>
+    </div>
+  </div>
+  <div>
+    <ul class="lists">
+      <li class="list">
+        <span>
+          <svg aria-hidden="true" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.5 12.75l6 6 9-13.5" stroke-linejoin="round" stroke-linecap="round"></path>
+          </svg>
+        </span>
+        <p>
+        {data.sessions}
+        </p>
+      </li>
+      <li class="list">
+        <span>
+          <svg aria-hidden="true" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.5 12.75l6 6 9-13.5" stroke-linejoin="round" stroke-linecap="round"></path>
+          </svg>
+        </span>
+        <p>
+        {data.timePerSession}
+        </p>
+      </li>
+      <li class="list">
+        <span>
+          <svg aria-hidden="true" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.5 12.75l6 6 9-13.5" stroke-linejoin="round" stroke-linecap="round"></path>
+          </svg>
+        </span>
+        <p>
+        {data.description}
+        </p>
+      </li>
+    </ul>
+  </div>
+  <div class="button-container">
+    <Link to="/contact">
+    <button type="button" >
+      Buy Now
+    </button></Link>
+  </div>
+</div>);
           })}
-        </div>
-        </Row>
+        </Row></div>
       </Container>
     </HelmetProvider>
   );
 };
 
-
-{/* <Row className=" sec_sp">
-          <Col lg="5">
-            <h3 className="color_sec py-4">Plans</h3>
-          </Col>
-          <Col lg="7">
-            <table className="table caption-top">
-              <tbody>
-                {worktimeline.map((data, i) => {
-                  return (
-                    <tr key={i}>
-                      {data.img?(
-                      <img src={data.img} alt="" className="icons"/>):(<td></td>)}
-                      <th scope="row">{data.plan}</th>                     
-                      <td>{data.details}</td>
-                      <td>{data.sessions}</td>
-                      <td>{data.cost}</td>                     
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Col>
-        </Row> */}
